6ed3ec17c4a93a5453f76de4d90f8a7dbb8d6ee9
Tiago Francisco <tiagolopesfrancisco@gmail.com>
2023-04-12 17:19:41 +0100

    battleground view

---
+++ src/gameViews/battleground/Battleground.js
@@ -0,0 +1,40 @@
+import { Fragment } from "react";
+import "./Battleground.css";
+
+export function Battleground(
+  currentMonster,
+  monsterHealth,
+  monsterDamage,
+  playerLevel,
+  playerHealth,
+  playerDamage,
+  playerClass
+) {
+  return (
+    <Fragment className="view-battleground">
+      <div className="battleground-page-tittle">
+        <h1>💀 Battlegrounds 💀</h1>
+      </div>
+      <div className="pve-panel">
+        <h2 className="h2-tittle">Battlefield</h2>
+        <div className="pve-board">
+          <div className="monster-info">
+            <h2>{currentMonster.name}</h2>
+            <p>Level: {currentMonster.level}</p>
+            <p>
+              Health: {monsterHealth} / {currentMonster.health}
+            </p>
+            <p>Attack Damage: {monsterDamage}</p>
+          </div>
+          <div className="player-info">
+            <h2>Player</h2>
+            <p>Level: {playerLevel}</p>
+            <p>Health: {playerHealth} / 200</p>
+            <p>Attack Damage: {playerDamage}</p>
+            <p>Player Class: {playerClass}</p>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+}
